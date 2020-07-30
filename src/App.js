cc5d881f8a4f91bd1be6749a7602a1300a1d9902
Olivia Robinson <thelibster123@gmail.com>
2020-07-30 14:02:25 +1200

    checklist2

--- src/App.js
+++ src/App.js
@@ -6,26 +6,66 @@ class App extends Component{
 
   constructor(props){
     super(props)
+    this.state = {
+      items : [
+        {
+          id:1,
+          content: 'Milk'
+        },
+        {
+          id:2,
+          content: 'Bread'
+        },
+        {
+          id:3,
+          content: 'Honey'
+        },
+        {
+          id:4,
+          content: 'Butter'
+        },
+      ],
+      newItemInput:''
+    }
 
-    this.items = [
-      {
-        id:1,
-        content: 'Milk'
-      },
-      {
-        id:2,
-        content: 'Bread'
-      },
-      {
-        id:3,
-        content: 'Honey'
-      },
-      {
-        id:4,
-        content: 'Butter'
-      },
-    ]
   }
+  handleNewItemInputChange = (e)=>{
+
+    this.setState({
+      newItemInput:e.target.value
+    })
+  
+  }
+  handleNewButtonClick = (e)=>{
+    e.preventDefault()
+    
+    var newItem = {
+      id:Date.now(),
+      content: this.state.newItemInput
+    }
+
+    var newList = [newItem, ...this.state.items]
+
+    this.setState({
+      items:newList,
+      newItemInput:''
+    })
+  }
+
+
+  handleItemDelete = (e)=>{
+    console.log(e.target)
+    var itemToDelete = parseInt(e.target.id)
+    var items = this.state.items
+
+    var filteredItems = items.filter((item)=>{
+      return item.id != itemToDelete
+    })
+
+    this.setState({items:filteredItems})
+  }
+
+
   render(){
 
     return (
@@ -39,17 +79,26 @@ class App extends Component{
             <div className="main-container">
 
               {
-                this.items.map(function(item){
+                this.state.items.map((item) => {
                   return (
 
-                    <div className="imput-group">
+                    <div className="imput-group" key={item.id}>
                       <input type="checkbox" id="task" name="task"/>
                       <label htmlFor="task"> {item.content}</label>
-                  </div>
+                      <button id={item.id} className="delete-btn" onClick={this.handleItemDelete}><i className="fas fa-trash-alt"></i></button>
+                    </div>
                   )
                 })
               }
 
+            </div>
+            <div className="input-container">
+            <form>
+              <div className="form-group">
+                <input type="text" className="new-item-input" placeholder="New Note" value={this.state.newItemInput} onChange={this.handleNewItemInputChange}></input>
+              </div>
+              <button type="submit" className="btn" onClick={this.handleNewButtonClick}><i className="fas fa-plus"></i></button>
+            </form>
             </div>
         </main>
     </div>
